5cc546d7cc1019cf6f6345620d894d67bb7e8da6
EmilLundstedt <sti.emil.lundstedt@gmail.com>
2020-09-03 17:56:55 +0200

    Added a popup for pokedex

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 function Pokedex(props) {
+function closePokeDex(){
+    console.log("CLOSING")
+   
+}
+
     return (
-        <div className="pokedex">
-            {props.list}
+        <div className="modal" onClick={closePokeDex}>
+            <div className="pokedex">
+                {props.list}
+            </div>
         </div>
     )
 }
